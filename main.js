7b249c13dd8e27fb50f3d65a5b97a3be4730f95a
DevAidar <limepeach26@gmail.com>
2020-04-18 10:18:54 -0700

    Changed it into single line function

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 strs = ["cat", "The fox", "Cow jumped over the moon", "good day"]
 
-let maxVsMinString = (strings) => {
-    return strings.reduce((maxStrLength, str) => Math.max(maxStrLength, str.length), 0) - strings.reduce((minStrLength, str) => Math.min(minStrLength, str.length), strs[0].length)
-};
+let maxVsMinString = (strings) => strings.reduce((maxStrLength, str) => Math.max(maxStrLength, str.length), 0) - strings.reduce((minStrLength, str) => Math.min(minStrLength, str.length), strings[0].length)
 
 console.log(maxVsMinString(strs));
